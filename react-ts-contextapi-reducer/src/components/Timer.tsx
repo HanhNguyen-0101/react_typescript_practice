f0716c87d4ab740d4cd368b568c4be11dafde938
Hanh Nguyen <hanhnguyen.learning@gmail.com>
2024-03-01 17:07:53 +0700

    useEffect[]: running timer: setInterval

--- react-ts-contextapi-reducer/src/components/Timer.tsx
+++ react-ts-contextapi-reducer/src/components/Timer.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
+
+  const [remainningTime, setRemainningTime] = useState(duration * 1000);
+  const timerRef = useRef<number | null>(null);
+
+  if (remainningTime <= 0 && timerRef.current) {
+    clearInterval(timerRef.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainningTime(prevTime => prevTime - 50);
+    }, 50);
+    timerRef.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const remainningTimeFormat = (remainningTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p><progress max={duration * 1000} value={remainningTime} /></p>
+      <p>{remainningTimeFormat}</p>
     </Container>
   );
 }
